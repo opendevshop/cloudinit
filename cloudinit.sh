fba5c6a8d81862a670b8f2796bee1a9de105c460
Jon Pugh <jon@thinkdrop.net>
2018-06-29 13:41:34 -0400

    Update version and add email option

--- cloudinit.sh
+++ cloudinit.sh
@@ -2,7 +2,10 @@
 
 # Set the version of DevShop you want.
 # Releases: https://github.com/opendevshop/devshop/releases
-VERSION=1.0.0-rc3
+VERSION=1.3.4
+
+# Set to your email address. The devmaster user 1 account will be set to this mail, and you will get an email when it is ready.
+EMAIL=jon@thinkdrop.net
 
 # Set the hostname to a fully qualified domain name that you will point to this server's IP.
 HOSTNAME=devshop.domain.com
@@ -10,4 +13,4 @@ HOSTNAME=devshop.domain.com
 # Download and run the script.
 wget https://raw.githubusercontent.com/opendevshop/devshop/$VERSION/install.sh
 export HOME=/root
-bash install.sh --hostname=$HOSTNAME
+bash install.sh --hostname=$HOSTNAME --email=$EMAIL
